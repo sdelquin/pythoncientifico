044f25be6b57fc48bb0b2a5489ab1381e579ab96
Sergio Delgado Quintero <sdelquin@gmail.com>
2019-05-14 16:42:00 +0100

    Fix variable naming in numpy exercise

--- solutions/numpy/euler_product.py
+++ solutions/numpy/euler_product.py
@@ -3,9 +3,9 @@ import math
 import numpy as np
 
 theta = 30
-n = 20
+k = 20
 
-values = np.arange(1, n, dtype='float64')
+values = np.arange(1, k, dtype='float64')
 z = np.cos(theta / np.power(2, values))
 
 lhs = np.sin(theta) / theta
